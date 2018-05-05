190c42a50ed9d90a1a9eabec028d50a082c88935
chaoseternai <jamesho.AHS@gmail.com>
2018-05-04 17:30:04 -0700

    Completed export (no logic)

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ const YOUTUBE_API_KEY = 'AIzaSyBgV3q8M71c-Hkqh3VmKzJSDlnoidrmuzM';
 
 const CB_URL = 'http%3A%2F%2F192.168.1.207%3A8000';
 
+let user_id = '';
 let token = '';
+let playlist_id = '';
 let song_names = [];
+let spotify_obj = [];
 let playlist_len = 0;
 let global_i = 0;
+
+function requestUserId(auth_token) {
+  $.ajax({
+  	url: `https://api.spotify.com/v1/me`,
+  	dataType: 'json',
+  	beforeSend: function(xhr){
+  		xhr.setRequestHeader('Accept', 'application/json');
+  		xhr.setRequestHeader('Content-Type', 'application/json');
+  		xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+  	},
+  	success: getUserId
+  }).fail(function(error) {
+    // handle failed api requests
+    console.log(error);
+  });
+}
+
+function getUserId(data) {
+  user_id = data.id;
+  console.log(`The id is ${user_id}`);
+}
 
 function initHTML() {
   const hash = window.location.hash
@@ -24,15 +48,19 @@ function initHTML() {
   }, {});
   window.location.hash = '';
   let _token = hash.access_token;
-  console.log(hash.expires_in);
 
   if(!_token) {
+    let scopes = encodeURIComponent('playlist-modify-public playlist-modify-private');
+
     $('#login').text('Log In');
-    $('#login').attr('href', `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_API_KEY}&response_type=token&redirect_uri=${CB_URL}`);
+    $('#login').attr('href', `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_API_KEY}&response_type=token&redirect_uri=${CB_URL}&scope=${scopes}`);
     $('#intro-screen').css('display', 'flex');
   } else {
     token = _token;
-    console.log(`The token is ${_token}.`);
+    user_id = requestUserId(token);
+
+    console.log(`This token will expire in ${hash.expires_in} seconds`);
+    console.log(`The token is ${token}`);
 
     $('#login').text('Profile');
     $('#login').attr('href', 'https://www.spotify.com/us/account/overview/');
@@ -109,7 +137,7 @@ function spotifySearch(raw_song) {
     .replace(/([\(|\[]Official(.*))/g, '')
     .replace(/([\(|\[](.*))/g, '')
     .replace('(Audio)', '');
-  console.log(song);
+  // console.log(song);
 
   $.ajax({
   	url: `https://api.spotify.com/v1/search?q=${encodeURIComponent(song)}&type=track&limit=1&offset=0`,
@@ -128,32 +156,57 @@ function spotifySearch(raw_song) {
 
 function appendSpotifyList(data) {
   let json = JSON.stringify(data, null, '\t');
-  console.log(data);
+  // console.log(data);
 
   let result = data.tracks.items[0];
   let title = "";
   let thumbnail_url = "";
 
   if(result) {
-    title = data.tracks.items[0].name;
-    thumbnail_url = data.tracks.items[0].album.images[1].url;
-  }
+    title = result.name;
+    thumbnail_url = result.album.images[1].url;
 
-  $('#spotify-list').append(`
-    <li>
-      <div class="song-number">${global_i+1}</div>
-      <img class="thumbnail" src='${thumbnail_url}' alt="Spotify Thumbnail">
-      <div class="song-name">${title}</div>
-      <input type="checkbox">
-    </li>
-  `);
+    spotify_obj.push({
+      name: result.name,
+      artist: result.artists[0].name,
+      thumbnail_url: result.album.images[1].url,
+      uri: result.uri,
+      valid: true,
+      selected: true
+    });
+
+    $('#spotify-list').append(`
+      <li>
+        <div class="song-number">${global_i+1}</div>
+        <img class="thumbnail" src='${spotify_obj[global_i].thumbnail_url}' alt="Spotify Album Thumbnail">
+        <div class="song-name">${spotify_obj[global_i].name}</div>
+        <input type="checkbox">
+      </li>
+    `);
+  } else {
+    spotify_obj.push({
+      valid: false,
+    })
+
+    $('#spotify-list').append(`
+      <li>
+        <div class="song-number">${global_i+1}</div>
+        <img class="thumbnail" src='${thumbnail_url}' alt="Invalid Album Cover">
+        <div class="song-name">Invalid Song</div>
+        <input type="checkbox">
+      </li>
+    `);
+  }
 
   if(global_i < playlist_len-1) {
     $('#spotify-list').append('<hr>');
   } else {
     $('#youtube-list').css('display', 'block');
     $('#spotify-list').css('display', 'block');
+    $('#spotify-export').css('display', 'block');
     $('footer').removeClass('fixed-footer');
+
+    console.log(spotify_obj);
   }
 
   global_i++;
@@ -161,9 +214,92 @@ function appendSpotifyList(data) {
     spotifySearch(song_names[global_i]);
 }
 
+function createPlaylist() {
+  $('#spotify-export').submit(function(event) {
+    event.preventDefault();
+
+    $.ajax({
+      url: `https://api.spotify.com/v1/users/${user_id}/playlists`,
+      data: JSON.stringify({
+        "name": $('#playlist-name').val(),
+        "description": "Playlist created through Playlist Converter",
+        "public": false
+      }),
+      dataType: 'json',
+      beforeSend: function(xhr){
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+      },
+      success: getPlaylistId,
+      type: 'POST'
+    }).fail(function(error) {
+      // handle failed api requests
+      console.log(error);
+    });
+  });
+}
+
+function getPlaylistId(data) {
+  playlist_id = data.id;
+  console.log(`Playlist successfully created`);
+  console.log(data);
+
+  addSongs();
+}
+
+function getAllSongUri() {
+  let uris = '';
+
+  for(let i=0; i<playlist_len; i++) {
+    if(spotify_obj[i].valid ) {
+      if(i > 0)
+        uris += ',';
+      uris += spotify_obj[i].uri;
+    }
+  }
+
+  return uris;
+}
+
+function addSongs() {
+  console.log('Adding songs');
+
+  let uris = getAllSongUri();
+  console.log(uris);
+
+  $.ajax({
+    url: `https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}/tracks?uris=${uris}`,
+    dataType: 'json',
+    beforeSend: function(xhr){
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+    },
+    success: completeConversion,
+    type: 'POST'
+  }).fail(function(error) {
+    // handle failed api requests
+    console.log(error);
+  });
+}
+
+function completeConversion(data) {
+  console.log(`Songs successfully added`);
+}
+
 function functionHandler() {
+  // This function handles what content is displayed based on if a user has
+  // logged in or not
   initHTML();
+
+  // This function creates an event listener that handles an API call to get
+  // the contents of a youtube playlist
   youtubeSearch();
+
+  // This function creates an event listener that handles an API call to
+  // create a spotify playlist and the export process
+  createPlaylist();
 }
 
 $(functionHandler);
